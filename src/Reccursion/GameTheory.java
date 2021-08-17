f51021af6c03aa116c9a1e6a7bd13198e15c6bcf
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2021-08-17 18:19:20 +0530

    qs added

--- src/Reccursion/GameTheory.java
+++ src/Reccursion/GameTheory.java
@@ -2,14 +2,13 @@ package Reccursion;
 
 public class GameTheory {
     public static void main(String[] args) {
-        int[] c = {5,3,7,10};
-        System.out.println(maxCoins(c,0,3));
-
-
+        int[] c = {5, 3, 7, 10};
+        System.out.println(maxCoins(c, 0, 3));
     }
-    public static int maxCoins(int[] coins, int l , int r){
-        if(l+1 == r) return Math.max(coins[l], coins[r]);
-        return Math.max(coins[l] + Math.min(maxCoins(coins,l+2,r),maxCoins(coins,l+1,r-1)),
-                coins[r] + Math.min(maxCoins(coins,l+1,r-1),maxCoins(coins,l,r-2)));
+
+    public static int maxCoins(int[] coins, int l, int r) {
+        if (l + 1 == r) return Math.max(coins[l], coins[r]);
+        return Math.max(coins[l] + Math.min(maxCoins(coins, l + 2, r), maxCoins(coins, l + 1, r - 1)),
+                coins[r] + Math.min(maxCoins(coins, l + 1, r - 1), maxCoins(coins, l, r - 2)));
     }
 }
