7a4b8858fb5ee0a22c7ab0b92669c5f578eaee10
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2021-08-21 11:03:46 +0530

    HotstarDesney.java question solved

--- src/NEW/HotstarDesney.java
+++ src/NEW/HotstarDesney.java
@@ -4,7 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Q.   find minimum value x in which
+ * Q1.  method minX
+ *      find minimum value x in which
  *      if we add each element of a List<Integer> from left to right, the sum never go below 1
  */
 
@@ -17,6 +18,10 @@ public class HotstarDesney {
         l.add(6);l.add(-9);l.add(3);l.add(15);
 
         System.out.println(minX(l));
+
+
+        int[] arr = {0, 1, 1, 0, 0, 1, 0, 0, 0, 0};
+        System.out.println(numOfClick(arr,30));
     }
    public static int minX(List<Integer> arr){
         int n = arr.size();
@@ -30,5 +35,39 @@ public class HotstarDesney {
             }
         }
         return -x + 1;
+    }
+
+
+
+    public static int numOfClick(int[] arr, int n){
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0 ; i<10; i++){
+            if(arr[i] == 1) list.add(i);
+        }
+        int[] ans = {0};
+        numOfClick(list,n,1,(int)Math.sqrt(n),ans,0);
+        return ans[0];
+    }
+
+    public static void numOfClick(List<Integer> arr, int n, int start, int end, int[] ans,int toAdd){
+        if(start > end) return;
+        if(n%start == 0 && isContain(arr,n/start)){
+                ans[0] = ans[0] + numOfDigit(n/start)+toAdd;
+                if(start != 1)numOfClick(arr,start,1,(int)Math.sqrt(start),ans,2);
+        }
+        numOfClick(arr, n,start+1,end,ans,0);
+    }
+
+    public static boolean isContain(List<Integer> arr, int n){
+        while(n > 0){
+            if(!arr.contains(n%10)) return false;
+            n = n/10;
+        }
+        return true;
+    }
+
+    public static int numOfDigit(int n){
+        if(n<10) return 1;
+        return 1 + numOfDigit(n/10);
     }
 }
--- src/NEW/Network/Main.java
+++ src/NEW/Network/Main.java
@@ -1,10 +1,13 @@
 package NEW.Network;
 
+import NEW.JSON;
+import netscape.javascript.JSException;
+import netscape.javascript.JSObject;
+
 public class Main {
     public static void main(String[] args) throws Exception {
         Networking n = new Networking();
-        String js = n.getText("https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&per_page=20&page=1&api_key=6f102c62f41998d151e5a1b48713cf13&format=json&nojsoncallback=1&extras=url_s");
+        String js = n.getText("https://www.instagram.com/ihrsachin");
         System.out.println(js);
-
     }
 }
