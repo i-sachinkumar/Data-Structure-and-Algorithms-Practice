ce9e2b79899cfe2f4c6d52bb9a129cabf8f6f16e
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2021-09-22 12:35:19 +0530

    solved stock buy and sell to maximize profit

---
+++ src/Arrays/Stock_buy_and_sell.java
@@ -0,0 +1,43 @@
+package Arrays;
+
+
+import kotlin.collections.ArrayDeque;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ *
+ * Given array of prices
+ * buy a stock on any day and sell it to any other day after buying to maximize profit
+ * O(N)
+ */
+
+public class Stock_buy_and_sell {
+    public static void main(String[] args) {
+        List<Integer> prices = Arrays.asList(3, 5, 9, 7, 1, 3, 12, 1, 3);
+
+        System.out.println(stockmax(prices));
+    }
+
+    public static long stockmax(List<Integer> p) {
+        int n = p.size();
+
+        int min = p.get(0);
+        int max = p.get(0);
+
+        long maxProfit = 0;
+
+        for(int  i = 1 ; i < n ; i++){
+            min = Math.min(min, p.get(i));
+            if(min == p.get(i)){
+                max = p.get(i);
+            }
+            max = Math.max(max,p.get(i));
+            maxProfit = Math.max(maxProfit,(max - min));
+        }
+
+        return maxProfit;
+    }
+}
