138ae17bf1c9503730eeeb9e6e65bb099948f2b2
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2021-12-14 19:37:03 +0530

    14 dec 2021 (sunday test)

---
+++ src/daily_test/Dec13/SingleNumber.java
@@ -0,0 +1,21 @@
+package daily_test.Dec13;
+
+// Qs Link = {https://leetcode.com/problems/single-number/}
+
+public class SingleNumber {
+    public static void main(String[] args) {
+        int[] nums = {1,2,4,4,1,6,2,9,8,9,8};
+        System.out.println(singleNumber(nums));
+    }
+
+    public static int singleNumber(int[] nums) {
+        // 0 ^ n = n
+        // n ^ n = 0
+        // so xor of all paired num will be zero ans xor of zero and single num will be that single num
+        int ans = 0;
+        for (int num : nums) {
+            ans = ans ^ num;
+        }
+        return ans;
+    }
+}
