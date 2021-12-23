7871817322a96f3334996b2c5eae1f1402477c33
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2021-12-23 12:33:12 +0530

    hackerrank qs

---
+++ src/Hackerrank/SurfaceArea3d.java
@@ -0,0 +1,65 @@
+package Hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SurfaceArea3d {
+    public static void main(String[] args) {
+        List<List<Integer>> A = new ArrayList<>();
+//        1 3 4
+//        2 2 3
+//        1 2 4
+        A.add(List.of(1,3,4));
+        A.add(List.of(2,3,3));
+        A.add(List.of(1,2,4));
+
+        System.out.println(surfaceArea(A));
+        System.out.println(surfaceArea2(A));
+
+    }
+
+    public static int surfaceArea(List<List<Integer>> A) {
+        int r = A.size();
+        int c = A.get(0).size();
+        int area = 2*r*c;
+        int[][] grid = new int[r+2][c+2];
+        for(int i = 1 ; i <= r ; i++){
+            for (int j = 1 ; j <= c ; j++){
+                grid[i][j] = A.get(i-1).get(j-1);
+            }
+        }
+
+        for(int i = 1 ; i <= r ; i++){
+            for (int j = 1 ; j <= c ; j++){
+                area += Math.max(0, grid[i][j] - grid[i][j+1]);
+                area += Math.max(0, grid[i][j] - grid[i][j-1]);
+                area += Math.max(0, grid[i][j] - grid[i+1][j]);
+                area += Math.max(0, grid[i][j] - grid[i-1][j]);
+            }
+        }
+        return area;
+
+    }
+
+    public static int surfaceArea2(List<List<Integer>> A) {
+        int r = A.size();
+        int c = A.get(0).size();
+        int area = 2*r*c;
+        int[][] grid = new int[r+1][c+1];
+        for(int i = 0 ; i < r ; i++){
+            for (int j = 0 ; j < c ; j++){
+                grid[i][j] = A.get(i).get(j);
+            }
+        }
+
+        for(int i = 0 ; i < r ; i++){
+            for (int j = 0 ; j < c ; j++){
+                area += (4 * grid[i][j]);
+                area -= (2 * Math.min(grid[i][j], grid[i+1][j]));
+                area -= (2 * Math.min(grid[i][j], grid[i][j+1]));
+            }
+        }
+        return area;
+
+    }
+}
