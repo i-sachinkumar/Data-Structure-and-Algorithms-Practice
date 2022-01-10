61668c8a062dbaf978ba9353357f30dca52c2fae
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2022-01-10 15:18:06 +0530

    dp and file handling

--- src/FileHandling/Count.java
+++ src/FileHandling/Count.java
@@ -1,7 +1,6 @@
 package FileHandling;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+
 import java.util.*;
 
 public class Count {
@@ -9,87 +8,163 @@ public class Count {
 
 
     public static void main(String[] args) {
-        String[][] liked = null;
-        String[][] disliked = null;
 
-        Set<String> set = new HashSet<>();
-        List<String> total_liked_items = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
 
-        try {
-            File myObj = new File("C:\\Users\\rjskg\\OneDrive\\Desktop\\input_data\\c_coarse.in.txt");
-            Scanner sc = new Scanner(myObj);
 
-            int c = sc.nextInt(); // num of customers
-            liked  = new String[c][5];
-            disliked  = new String[c][5];
+        int t = sc.nextInt();
+        sc.nextLine();
 
-            for (int i = 0 ; i < c ; i++){
+        while (t > 0) {
 
-                int l = sc.nextInt();       //num of liked items
+            String s = sc.nextLine();
 
-                for(int j = 0 ; j < l ; j++){
-                    String ingredient =  sc.next();
-                    liked[i][j] = ingredient;
-                    if(set.add(ingredient)) total_liked_items.add(ingredient);
+
+            boolean isFound = false;
+
+            for (int i = 0; i < s.length(); i++) {
+                for (int j = i + 1; j <= s.length(); j++) {
+                    String binary = s.substring(i, j);
+
+                    int num = binToDec(binary);
+
+                    if (isPrime(num)) {
+                        System.out.println("Yes");
+                        isFound = true;
+                        break;
+                    }
                 }
-
-
-                int d = sc.nextInt();  // num of disliked item
-                for(int j = 0 ; j < d ; j++){
-                    String ingredient =  sc.next();
-                    disliked[i][j] = ingredient;
-                }
+                if(isFound) break;
             }
 
-
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+            if (!isFound) System.out.println("No");
+            t--;
         }
-
-
-        int numOfItems = 0;
-        List<String> list = new ArrayList<>();
-
-        try {
-            File myObj = new File("C:\\Users\\rjskg\\OneDrive\\Desktop\\input_data\\c_coarse.out.txt");
-            Scanner sc = new Scanner(myObj);
-
-            numOfItems = sc.nextInt();
-
-            for(int i = 0 ; i < numOfItems ; i++){
-                list.add(sc.next());
-            }
-            sc.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
-
-        int count = 0;
-
-        for(int i = 0 ; i< liked.length ; i++){
-            if(canBuy(liked[i], disliked[i], list)) count++;
-        }
-
-        System.out.println(count);
 
     }
 
+    static int binToDec(String s){
+        int ans = 0;
 
-    static boolean canBuy(String[] buyerLikes, String[] buyerDislikes, List<String> target_items){
-        for(String item : buyerLikes){
-            if(item == null || item == "") break;
-            if(!target_items.contains(item)) return false;
+        int pow = 0;
+        for(int i = s.length()-1; i>=0 ; i--){
+            if(s.charAt(i) == '1'){
+                ans += (int)Math.pow(2, pow);
+            }
+            pow++;
         }
+        return ans;
+    }
 
-        for (String item : buyerDislikes){
-            if(item == null || item == "") break;
-            else if(target_items.contains(item)) return false;
+    static boolean isPrime(int n){
+        if(n == 0) return false;
+        if(n ==1) return false;
+        if(n == 2 ) return true;
+
+        for(int i = 2; i <= (int)Math.sqrt(n); i++){
+            if(n%i == 0) return false;
         }
 
         return true;
     }
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//
+//
+//
+//
+//        String[][] liked = null;
+//        String[][] disliked = null;
+//
+//        Set<String> set = new HashSet<>();
+//        List<String> total_liked_items = new ArrayList<>();
+//
+//        try {
+//            File myObj = new File("C:\\Users\\rjskg\\OneDrive\\Desktop\\input_data\\d_difficult.in.txt");
+//            Scanner sc = new Scanner(myObj);
+//
+//            int c = sc.nextInt(); // num of customers
+//            liked  = new String[c][5];
+//            disliked  = new String[c][5];
+//
+//            for (int i = 0 ; i < c ; i++){
+//
+//                int l = sc.nextInt();       //num of liked items
+//
+//                for(int j = 0 ; j < l ; j++){
+//                    String ingredient =  sc.next();
+//                    liked[i][j] = ingredient;
+//                    if(set.add(ingredient)) total_liked_items.add(ingredient);
+//                }
+//
+//
+//                int d = sc.nextInt();  // num of disliked item
+//                for(int j = 0 ; j < d ; j++){
+//                    String ingredient =  sc.next();
+//                    disliked[i][j] = ingredient;
+//                }
+//            }
+//
+//
+//            sc.close();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
+//
+//
+//        int numOfItems = 0;
+//        List<String> list = new ArrayList<>();
+//
+//        try {
+//            File myObj = new File("C:\\Users\\rjskg\\OneDrive\\Desktop\\input_data\\d_difficult_3.out.txt");
+//            Scanner sc = new Scanner(myObj);
+//
+//            numOfItems = sc.nextInt();
+//
+//            for(int i = 0 ; i < numOfItems ; i++){
+//                list.add(sc.next());
+//            }
+//            sc.close();
+//        } catch (FileNotFoundException e) {
+//            System.out.println("An error occurred.");
+//            e.printStackTrace();
+//        }
+//
+//        int count = 0;
+//
+//        for(int i = 0 ; i< liked.length ; i++){
+//            if(canBuy(liked[i], disliked[i], list)) count++;
+//        }
+//
+//        System.out.println(count);
+//
+//    }
+//
+//
+//    static boolean canBuy(String[] buyerLikes, String[] buyerDislikes, List<String> target_items){
+//        for(String item : buyerLikes){
+//            if(item == null || item == "") break;
+//            if(!target_items.contains(item)) return false;
+//        }
+//
+//        for (String item : buyerDislikes){
+//            if(item == null || item == "") break;
+//            else if(target_items.contains(item)) return false;
+//        }
+//
+//        return true;
+//    }
 
 }
