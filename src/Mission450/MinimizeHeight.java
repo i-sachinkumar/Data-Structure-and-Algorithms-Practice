d101728964d5b87f5898de2c6dc452a09f40bbce
Sachin Kumar <81405044+i-sachinkumar@users.noreply.github.com>
2022-05-18 14:02:07 +0530

    mission 450 dsa qs

---
+++ src/Mission450/MinimizeHeight.java
@@ -0,0 +1,56 @@
+package Mission450;
+
+import java.util.*;
+
+
+/** GFG
+ * Given an array arr[] denoting heights of N towers and a positive integer K,
+ * you have to modify the height of each tower either by increasing or decreasing them by K only once.
+ * After modifying, height should be a non-negative integer.
+ * Find out the minimum possible difference of the height of shortest and longest towers
+ * after you have modified each tower.
+ */
+
+public class MinimizeHeight {
+    public static void main(String[] args) {
+
+        int k = 5;
+        int n = 10;
+        int[] arr = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
+
+        System.out.println(getMinDiff(arr, n, k));
+    }
+
+    static int getMinDiff(int[] arr, int n, int k) {
+        // code here
+        if(n == 1) return 0;
+
+        Arrays.sort(arr);
+
+        // curr diff without modification
+        int ans = arr[n-1] - arr[0];
+
+        // curr minimum value after sorting
+        int tempMin = arr[0];
+
+        // curr max value after sorting
+        int tempMax = arr[n-1];
+
+        for(int i = 1 ; i < n ; i++){
+
+            // if it is negative only one way of modification is possible
+            if(arr[i] - k >= 0){
+
+                //increasing starting value by k, then decreasing ith value, min val can change, so reassigned
+                tempMin = Math.min(arr[0] + k, arr[i] - k);
+
+                //increasing prev value by k, then decreasing last value, max val can change, so reassigned
+                tempMax = Math.max(arr[i-1] + k, arr[n-1] - k);
+
+                ans = Math.min(ans, tempMax - tempMin);
+            }
+        }
+
+        return ans;
+    }
+}
--- src/Sorting/QuickSort.java
+++ src/Sorting/QuickSort.java
@@ -1,13 +1,32 @@
 package Sorting;
 
+import java.util.*;
+
 public class QuickSort {
     public static void main(String[] args) {
-        int[] a = {100, -1, -72, 1, 3, 0, 8, 6};
-        quick_sort(a,0,7);
+        int[] a = {24, 9, 29, 14, 19, 27, 24};
 
-        for(int i : a){
-            System.out.print(i + " ");
-        }
+//        quick_sort(a,0,7);
+//
+//        for(int i : a){
+//            System.out.print(i + " ");
+//        }
+        Arrays.toString(a);
+        String s = "eklvejvn";
+        System.out.println(String.valueOf(s));
+        PriorityQueue<Integer> q = new PriorityQueue(Collections.reverseOrder());
+        q.add(2);
+        q.add(5);
+        q.add(0);
+
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+        System.out.println(q.poll());
+
+
+//        partition1(a, 0, a.length-1);
+//        System.out.println(Arrays.toString(a));
     }
 
     private static void quick_sort(int[] a, int l , int h) {
@@ -34,6 +53,31 @@ public class QuickSort {
         int temp = a[l];
         a[l] = a[j];
         a[j] = temp;
+
+        return j;
+    }
+
+
+
+
+    public static int partition1(int[] arr, int l, int h){
+        int i = l;
+        int j = h;
+        int pivot = arr[l];
+
+        while(i < j){
+            while (arr[i] <= pivot && i < h) i++;
+            while (arr[j] >= pivot && j > l) j--;
+            if(i < j){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[j];
+        arr[j] = arr[l];
+        arr[l] = temp;
 
         return j;
     }
